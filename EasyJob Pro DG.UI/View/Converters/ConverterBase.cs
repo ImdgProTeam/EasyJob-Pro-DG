553bb90bd508751becdf4a02253b55dbfdccd2f9
eblabs <eblabs@mail.ru>
2022-04-25 22:40:11 +0300

    View: Converters: Created ConverterBase class and CompositeConverter

---
+++ EasyJob Pro DG.UI/View/Converters/ConverterBase.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace EasyJob_ProDG.UI.View.Converters
+{
+    internal abstract class ConverterBase : IValueConverter
+    {
+        public abstract object Convert(object value, Type targetType, object parameter, CultureInfo culture);
+
+        public virtual object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotSupportedException("Conversion back is not supported");
+        }
+    }
+}
