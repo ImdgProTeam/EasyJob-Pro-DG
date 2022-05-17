f650562a80a672de000181705322c91e1d0d67db
eblabs <eblabs@mail.ru>
2022-05-17 06:25:03 +0300

    Class DgWrapper splitted to three partial class files

---
+++ EasyJob Pro DG.UI/Wrapper/DgWrapperMethods.cs
@@ -0,0 +1,212 @@
+﻿using EasyJob_ProDG.Model.Cargo;
+using EasyJob_ProDG.Model.IO;
+using EasyJob_ProDG.Model.Validators;
+using EasyJob_ProDG.UI.Messages;
+using EasyJob_ProDG.UI.Services.DialogServices;
+using EasyJob_ProDG.UI.Utility;
+using System;
+using System.Runtime.CompilerServices;
+
+namespace EasyJob_ProDG.UI.Wrapper
+{
+    public partial class DgWrapper : ModelWrapper<Dg>, ILocationOnBoard, IContainer, IUpdatable
+    {
+        #region Public methods
+        // --------------- Methods ---------------------------------------
+
+        /// <summary>
+        /// Clears and registrations and subscriptions, e.g. before deleting
+        /// </summary>
+        internal void ClearSubscriptions()
+        {
+            DataMessenger.Default.Unregister(this);
+        }
+
+        /// <summary>
+        /// Compiles location of input of bay, row or tier
+        /// </summary>
+        /// <param name="value">Bay, row or tier</param>
+        /// <param name="propertyName">Specified property</param>
+        /// <returns></returns>
+        internal string CompileLocation(byte value, [CallerMemberName] string propertyName = null)
+        {
+            switch (propertyName.ToLower())
+            {
+                case "bay":
+                    return AddZeroIfRequired(value, 2) + AddZeroIfRequired(Row) + AddZeroIfRequired(Tier);
+                case "row":
+                    return AddZeroIfRequired(Bay, 2) + AddZeroIfRequired(value) + AddZeroIfRequired(Tier);
+                case "tier":
+                    return AddZeroIfRequired(Bay, 2) + AddZeroIfRequired(Row) + AddZeroIfRequired(value);
+                default:
+                    return Location;
+            }
+        }
+
+        /// <summary>
+        /// Adds TechnicalName, if any, to ProperShippingName and changes status of IsTechnicalNameIncluded.
+        /// </summary>
+        internal void IncludeTechnicalName()
+        {
+            if (IsTechnicalNameIncluded) return;
+            if (string.IsNullOrEmpty(TechnicalName)) return;
+            Name += "\n" + TechnicalName;
+            IsTechnicalNameIncluded = true;
+        }
+
+        /// <summary>
+        /// Removes TechnicalName, if any, to ProperShippingName and changes status of IsTechnicalNameIncluded.
+        /// </summary>
+        public void RemoveTechnicalName()
+        {
+            if (!IsTechnicalNameIncluded) return;
+            if (string.IsNullOrEmpty(TechnicalName)) return;
+            Name = Name.Replace(TechnicalName, "");
+            if (Name.EndsWith("\n"))
+                Name = Name.Remove(Name.Length - 1);
+            IsTechnicalNameIncluded = false;
+        }
+        #endregion
+
+
+        #region Private methods
+        // --------------- Private methods ---------------------------------------
+
+        /// <summary>
+        /// Checks if input value is less then '10' and adds '0' before the digit if the case
+        /// </summary>
+        /// <param name="value"></param>
+        /// <param name="numberOfZeros"></param>
+        /// <returns></returns>
+        private string AddZeroIfRequired(byte value, byte numberOfZeros = 1)
+        {
+            string result = "";
+            if (value < 10) result += "0";
+            if (value < 100 && numberOfZeros == 2) result += "0";
+            result += value.ToString();
+            return result;
+        }
+
+        /// <summary>
+        /// Supporting method to parse multiple classes into array
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        private string[] ParseMultipleClasses(string value)
+        {
+            char[] separator = { ' ', ',' };
+            string[] array = value.Split(separator, StringSplitOptions.RemoveEmptyEntries);
+            return array;
+        }
+
+        /// <summary>
+        /// Checks if UN no exists in database and prompts user to confirm weather to continue if UN does not exist.
+        /// </summary>
+        /// <param name="unno">UN no being checked</param>
+        /// <returns>If UN no is valid or user acknowledge</returns>
+        private bool CheckForExistingUnno(ushort unno)
+        {
+            if (UnnoValidator.Validate(unno)) return true;
+            if (_messageDialogService.ShowYesNoDialog(
+                    $"UN no {unno:0000} does not exist in the DataBase. \nDo you wish to proceed?", "Attention!") ==
+                MessageDialogResult.Yes) return true;
+            return false;
+        }
+        #endregion
+
+
+        #region Methods affecting view changes
+        // --------------- Methods affecting view changes ---------------------------
+
+        /// <summary>
+        /// Updates Dg info from DgListDataBase for the unit and updates presentation
+        /// </summary>
+        private void UpdateDgInfoAndUploadChanges()
+        {
+            Model.UpdateDgInfo(_dgDataBase);
+            UpdateDgDataPresentation();
+        }
+
+        /// <summary>
+        /// Called when packing group is updated. Contains logic to process the PKG change.
+        /// </summary>
+        private void OnUpdatePackingGroup()
+        {
+            Model.AssignFromDgList(_dgDataBase, false, true);
+            OnDgPackingGroupChangedEventHandler.Invoke(this);
+            UpdateDgDataPresentation();
+        }
+
+        /// <summary>
+        /// Notifies CargoPlan of change in DgNetWeight.
+        /// </summary>
+        private void OnNetWeightChanged()
+        {
+            DataMessenger.Default.Send<UpdateCargoPlan>(new UpdateCargoPlan(), "Net weight changed");
+        }
+
+        /// <summary>
+        /// Invokes event to update conflict list
+        /// </summary>
+        private void UpdateConflictList()
+        {
+            OnConflictListToBeChangedEventHandler.Invoke(this);
+        }
+
+        private void UpdateDgStowageConflicts()
+        {
+            DataMessenger.Default.Send(new ConflictListToBeUpdatedMessage(this));
+        }
+
+        /// <summary>
+        /// Calls OnPropertyChanged for most of Dg properties
+        /// </summary>
+        private void UpdateDgDataPresentation()
+        {
+            OnPropertyChanged("Unno");
+            OnPropertyChanged("DgClass");
+            OnPropertyChanged("DgSubclass");
+            OnPropertyChanged("Name");
+            OnPropertyChanged("PackingGroup");
+            OnPropertyChanged("AllDgClasses");
+            OnPropertyChanged("Liquid");
+            OnPropertyChanged("Flammable");
+            OnPropertyChanged("EmitFlammableVapours");
+            OnPropertyChanged("StowageCat");
+            OnPropertyChanged("StowageSW");
+            OnPropertyChanged("SegregationSG");
+            OnPropertyChanged("SegregationGroup");
+            OnPropertyChanged("Special");
+            OnPropertyChanged("IsMax1L");
+            OnPropertyChanged("IsWaste");
+        }
+
+        /// <summary>
+        /// Calls OnPropertyChanged for all location related properties
+        /// </summary>
+        private void UpdateLocationPresentation()
+        {
+            OnPropertyChanged("Location");
+            OnPropertyChanged("Bay");
+            OnPropertyChanged("Row");
+            OnPropertyChanged("Tier");
+            OnPropertyChanged("HoldNr");
+            OnPropertyChanged("IsUnderdeck");
+            OnPropertyChanged("Size");
+            OnPropertyChanged("LocationSortable");
+        }
+
+        /// <summary>
+        /// Sends message to synchronise changes with CargoPlan
+        /// </summary>
+        /// <param name="value">new value set</param>
+        /// <param name="oldValue">old value</param>
+        /// <param name="propertyName">property that is changed</param>
+        private void SetToAllContainersInPlan(object value, object oldValue = null, [CallerMemberName] string propertyName = null)
+        {
+            DataMessenger.Default.Send(new CargoPlanUnitPropertyChanged(this, value, oldValue, propertyName));
+        }
+        #endregion
+
+    }
+}
---
+++ EasyJob Pro DG.UI/Wrapper/DgWrapperProperties.cs
@@ -0,0 +1,685 @@
+﻿using EasyJob_ProDG.Model.Cargo;
+using EasyJob_ProDG.Model.IO;
+using EasyJob_ProDG.UI.Data;
+using EasyJob_ProDG.UI.View.Sort;
+using System;
+
+namespace EasyJob_ProDG.UI.Wrapper
+{
+    public partial class DgWrapper : ModelWrapper<Dg>, ILocationOnBoard, IContainer, IUpdatable
+    {
+        //Private fields
+        private SortableLocation _locationSortable;
+
+
+        // --------------- Public properties ----------------------------
+
+        public bool IsInList { get; set; }
+
+
+        #region LocationOnBoard properties
+        //--------------- Properties related to container location ------------------
+
+        public bool IsUnderdeck
+        {
+            get { return GetValue<bool>(); }
+            set
+            {
+            }
+        }
+        public byte Bay
+        {
+            get { return GetValue<byte>(); }
+            set
+            { }
+        }
+        public byte Row
+        {
+            get { return GetValue<byte>(); }
+            set { }
+        }
+        public byte Size
+        {
+            get => GetValue<byte>();
+            set { }
+        }
+        public byte Tier
+        {
+            get { return GetValue<byte>(); }
+            set { }
+        }
+        public byte HoldNr
+        {
+            get { return GetValue<byte>(); }
+            set
+            {
+            }
+        }
+
+        /// <summary>
+        /// Container position on board (slot address)
+        /// </summary>
+        public string Location
+        {
+            get => GetValue<string>();
+            set
+            {
+                var oldValue = Location;
+                if (oldValue == value) return;
+
+                SetValue(value);
+                _locationSortable = null;
+                if (!IsInList) return;
+
+                if (CurrentProgramData.OwnShip != null)
+                    Model.HoldNr = CurrentProgramData.OwnShip.DefineCargoHoldNumber(Bay);
+                SetToAllContainersInPlan(GetValue<string>(), oldValue);
+                UpdateLocationPresentation();
+            }
+        }
+
+        /// <summary>
+        /// Property used to sort DgWrappers in chosen order
+        /// </summary>
+        public SortableLocation LocationSortable => _locationSortable ??= new SortableLocation(this);
+        #endregion
+
+
+        #region IUpdatable
+        //--------------- Properties related to IUpdatable -------------------------- 
+
+        public bool IsPositionLockedForChange
+        {
+            get => GetValue<bool>();
+            set
+            {
+                if (IsPositionLockedForChange == value) return;
+                SetValue(value);
+                SetToAllContainersInPlan(value);
+            }
+        }
+        public bool IsToBeKeptInPlan
+        {
+            get => GetValue<bool>();
+            set
+            {
+                if (IsToBeKeptInPlan == value) return;
+                SetValue(value);
+                SetToAllContainersInPlan(value);
+            }
+        }
+        public bool IsNotToImport
+        {
+            get => GetValue<bool>();
+            set
+            {
+                if (IsNotToImport == value) return;
+                SetValue(value);
+                SetToAllContainersInPlan(value);
+            }
+        }
+        public bool IsToImport
+        {
+            get => GetValue<bool>();
+            set
+            {
+                if (IsToImport == value) return;
+                SetValue(value);
+                SetToAllContainersInPlan(value);
+            }
+        }
+
+        //properties with empty setters
+        public bool IsNewUnitInPlan
+        {
+            get => GetValue<bool>();
+            set { }
+        }
+        public bool HasLocationChanged
+        {
+            get => GetValue<bool>();
+            set { }
+        }
+        public bool HasUpdated
+        {
+            get => GetValue<bool>();
+            set { }
+        }
+        public bool HasPodChanged
+        {
+            get => GetValue<bool>();
+            set { }
+        }
+        public bool HasContainerTypeChanged
+        {
+            get => GetValue<bool>();
+            set { }
+        }
+
+        public string LocationBeforeRestow
+        {
+            get => GetValue<string>(); set { }
+        }
+        #endregion
+
+
+        #region IContainer
+        //--------------- Properties related to IContainer --------------------------- 
+
+        /// <summary>
+        /// Container number
+        /// </summary>
+        public string ContainerNumber
+        {
+            get => GetValue<string>();
+            set
+            {
+                var oldValue = ContainerNumber;
+                if (oldValue == value) return;
+                SetValue(value.ToUpper());
+                SetToAllContainersInPlan(value.ToUpper(), oldValue);
+            }
+        }
+
+        /// <summary>
+        /// Port of loading
+        /// </summary>
+        public string POL
+        {
+            get => GetValue<string>();
+            set
+            {
+                if (POL == value) return;
+                SetValue(value);
+                if (IsInList)
+                {
+                    SetToAllContainersInPlan(value);
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        /// <summary>
+        /// Port of discharging
+        /// </summary>
+        public string POD
+        {
+            get => GetValue<string>();
+            set
+            {
+                if (POD == value) return;
+                SetValue(value);
+                if (IsInList)
+                {
+                    SetToAllContainersInPlan(value);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Port of final destination
+        /// </summary>
+        public string FinalDestination
+        {
+            get => GetValue<string>();
+            set
+            {
+                if (FinalDestination == value) return;
+                SetValue(value);
+                if (IsInList)
+                {
+                    SetToAllContainersInPlan(value);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Container type code
+        /// </summary>
+        public string ContainerType
+        {
+            get => GetValue<string>();
+            set
+            {
+                if (ContainerType == value.ToUpper()) return;
+                SetValue(value.ToUpper());
+                if (IsInList)
+                {
+                    Model.UpdateContainerType();
+                    SetToAllContainersInPlan(value.ToUpper());
+
+                    OnPropertyChanged($"IsClosed");
+                    OnPropertyChanged($"IsOpen");
+                }
+            }
+        }
+
+        /// <summary>
+        /// True if unit is a Closed Freight Container
+        /// </summary>
+        public bool IsClosed
+        {
+            get => GetValue<bool>();
+            set
+            {
+                if (IsClosed == value) return;
+                SetValue(value);
+                SetToAllContainersInPlan(value);
+
+                OnPropertyChanged($"IsOpen");
+            }
+        }
+
+        /// <summary>
+        /// True if container is of open type. Opposite to 'IsClosed'
+        /// </summary>
+        public bool IsOpen
+        {
+            get => !IsClosed;
+            set => IsClosed = !value;
+        }
+
+        /// <summary>
+        /// True if the container is a live reefer
+        /// </summary>
+        public bool IsRf
+        {
+            get => GetValue<bool>();
+            set
+            {
+                if (IsRf == value) return;
+                SetValue(value);
+                SetToAllContainersInPlan(value);
+            }
+        }
+
+        /// <summary>
+        /// Container operator
+        /// </summary>
+        public string Carrier
+        {
+            get => GetValue<string>();
+            set
+            {
+                if (Carrier == value) return;
+                SetValue(value);
+                if (IsInList)
+                    SetToAllContainersInPlan(value);
+            }
+        }
+
+        public bool ContainerTypeRecognized { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        #endregion
+
+
+        #region Dg properties with complex functionality
+        //--------------- Properties related to Dangerous Cargo ---------------------
+
+        /// <summary>
+        /// UN no of a dangerous good
+        /// </summary>
+        public ushort Unno
+        {
+            get => GetValue<ushort>();
+            set
+            {
+                if (Unno == value) return;
+                if (!CheckForExistingUnno(value)) return;
+
+                SetValue(value);
+                if (value != 1950) IsMax1L = false;
+                UpdateDgInfoAndUploadChanges();
+                UpdateConflictList();
+            }
+        }
+
+        /// <summary>
+        /// Set: adds string dg class to dgclass and will update allDgClasses.
+        /// Get: returns string with dgclass.
+        /// </summary>
+        public string DgClass
+        {
+            get => GetValue<string>();
+            set
+            {
+                string[] tempdgsubclasses = Model.DgSubclassArray;
+                Model.ClearAllDgClasses();
+                SetValue(value);
+                foreach (var dgsubclass in tempdgsubclasses)
+                {
+                    Model.DgSubclass = dgsubclass;
+                }
+                if (IsInList)
+                {
+                    OnPropertyChanged($"AllDgClasses");
+                    UpdateConflictList();
+                }
+            }
+        }
+
+        /// <summary>
+        /// Set: will add string dg class to dgsubclass and will update allDgClasses.
+        /// Get: will return string with all subclasses listed and separated with comma.
+        /// </summary>
+        public string DgSubclass
+        {
+            get => GetValue<string>();
+            set
+            {
+                string tempDgClass = DgClass;
+                Model.ClearAllDgClasses();
+                Model.DgClass = tempDgClass;
+
+                string[] array = ParseMultipleClasses(value);
+                foreach (var val in array)
+                {
+                    if (!string.IsNullOrEmpty(val))
+                        SetValue(val);
+                }
+
+                if (IsInList)
+                {
+                    OnPropertyChanged($"AllDgClasses");
+                    UpdateConflictList();
+                }
+            }
+        }
+
+        /// <summary>
+        /// Set: will parse string with packing group and record it to dgpg.
+        /// Get: will return string containing better view of a packing group.
+        /// </summary>
+        public string PackingGroup
+        {
+            get { return GetValue<string>(); }
+            set
+            {
+                SetValue(value);
+                if (IsInList)
+                {
+                    OnUpdatePackingGroup();
+                    UpdateConflictList();
+                }
+            }
+        }
+
+        public decimal DgNetWeight
+        {
+            get { return GetValue<decimal>(); }
+            set
+            {
+                SetValue(value);
+                OnNetWeightChanged();
+            }
+        }
+
+        public bool IsLq
+        {
+            get => GetValue<bool>();
+            set
+            {
+                SetValue(value);
+                UpdateConflictList();
+            }
+        }
+
+        public bool IsMp
+        {
+            get { return GetValue<bool>(); }
+            set
+            {
+                SetValue(value);
+                UpdateDgStowageConflicts();
+            }
+        }
+
+        public bool IsMax1L
+        {
+            get => GetValue<bool>();
+            set
+            {
+                if (IsMax1L == value) return;
+                if (value && Unno != 1950) return;
+
+                SetValue(value);
+                if (value)
+                {
+                    if (!Name.ToLower().Replace(" ", "").Contains("max1l")) Name += ", Max 1L";
+                    if (IsWaste) IsWaste = false;
+                }
+                else
+                {
+                    Name = Name.Replace(", Max 1L", "");
+                }
+
+                UpdateConflictList();
+            }
+        }
+
+        public bool IsWaste
+        {
+            get => GetValue<bool>();
+            set
+            {
+                if (IsWaste == value) return;
+                SetValue(value);
+
+                if (value)
+                {
+                    if (Unno == 1950) IsMax1L = false;
+                }
+
+                UpdateDgStowageConflicts();
+
+                OnPropertyChanged("StowageCat");
+                OnPropertyChanged("StowageSW");
+                OnPropertyChanged("Name");
+            }
+        }
+        public bool IsStabilized
+        {
+            get { return GetValue<bool>(); }
+            set
+            {
+                if (IsStabilized == value) return;
+                SetValue(value);
+
+                UpdateDgStowageConflicts();
+
+                OnPropertyChanged("StowageCat");
+                OnPropertyChanged("StowageSW");
+                OnPropertyChanged("Name");
+            }
+        }
+
+        /// <summary>
+        /// Proper shipping name
+        /// </summary>
+        public string Name
+        {
+            get { return GetValue<string>(); }
+            set
+            {
+                string oldName = Name.ToLower();
+                string newName = value.ToLower().Replace(" ", "");
+
+                SetValue(value);
+                IsNameChanged = !string.Equals(OriginalNameFromCode, value);
+
+                if (oldName.Contains("waste") && !newName.Contains("waste"))
+                    IsWaste = false;
+                else if (newName.Contains("waste")) IsWaste = true;
+                else if (oldName.Contains("max1l") && !newName.Contains("max1l"))
+                    IsMax1L = false;
+                else if (newName.Contains("max1l")) IsMax1L = true;
+                else if (oldName.Contains("stabilized") && !newName.Contains("stabilized"))
+                    IsStabilized = false;
+                else if (newName.Contains("stabilized")) IsStabilized = true;
+
+                // AS COOLANT OR AS CONDITIONER 5.5.3.2.1
+                if (oldName.Contains("coolant") && !newName.Contains("coolant") ||
+                    oldName.Contains("conditioner") && !newName.Contains("conditioner") ||
+                    !oldName.Contains("coolant") && newName.Contains("coolant") ||
+                    !oldName.Contains("conditioner") && newName.Contains("conditioner"))
+                    UpdateConflictList();
+            }
+        }
+
+        public bool IsSelfReactive
+        {
+            get { return GetValue<bool>(); }
+            set
+            {
+                SetValue(value);
+                OnPropertyChanged();
+            }
+        }
+
+        public char StowageCat
+        {
+            get { return GetValue<char>(); }
+            set
+            {
+                SetValue(value);
+                UpdateDgStowageConflicts();
+            }
+        }
+        #endregion
+
+
+        #region Dg properties without additional functionality
+        // --------- Dg properties without additional functionality -------
+
+        public Conflicts Conflicts
+        {
+            get { return GetValue<Conflicts>(); }
+            set { SetValue(value); }
+        }
+        public string DgEMS
+        {
+            get
+            {
+                return GetValue<string>();
+            }
+            set
+            {
+                SetValue(value);
+            }
+        }
+        public string EmergencyContacts
+        {
+            get => GetValue<string>();
+            set => SetValue(value);
+        }
+        public bool EmitFlammableVapours
+        {
+            get { return GetValue<bool>(); }
+            set
+            {
+                SetValue(value);
+            }
+        }
+        public bool Flammable
+        {
+            get { return GetValue<bool>(); }
+            set
+            {
+                SetValue(value);
+            }
+        }
+        public string FlashPoint
+        {
+            get { return GetValue<string>(); }
+            set
+            {
+                SetValue(value);
+            }
+        }
+
+        /// <summary>
+        /// True if proper shipping name is different from original from DgList.
+        /// </summary>
+        public bool IsNameChanged
+        {
+            get
+            {
+                return GetValue<bool>();
+            }
+            set
+            {
+                SetValue(value);
+            }
+        }
+        public bool IsTechnicalNameIncluded
+        {
+            get
+            {
+                return GetValue<bool>();
+            }
+            set
+            {
+                SetValue(value);
+            }
+        }
+        public bool Liquid
+        {
+            get { return GetValue<bool>(); }
+            set
+            {
+                SetValue(value);
+            }
+        }
+        public string NumberAndTypeOfPackages
+        {
+            get { return GetValue<string>(); }
+            set { SetValue(value); }
+        }
+        public string Remarks
+        {
+            get => GetValue<string>();
+            set => SetValue(value);
+        }
+        public string SegregationGroup
+        {
+            get { return GetValue<string>(); }
+            set { SetValue(value); }
+        }
+
+        /// <summary>
+        /// Technical name
+        /// </summary>
+        public string TechnicalName
+        {
+            get { return GetValue<string>(); }
+            set
+            {
+                SetValue(value);
+            }
+        }
+        #endregion
+
+
+        #region Readonly Dg properties
+        // ---------- Readonly Dg properties ------------------------------
+
+        public bool IsConflicted => GetValue<bool>();
+        public string AllDgClasses => GetValue<string>();
+        public string Properties => GetValue<string>();
+        public string SegregationSG
+        {
+            get { return GetValue<string>(); }
+        }
+        public string Special
+        {
+            get { return GetValue<string>(); }
+        }
+        public string StowageSW
+        {
+            get { return GetValue<string>(); }
+        }
+        public string Surrounded => GetValue<string>();
+        public string OriginalNameFromCode => GetValue<string>();
+
+        #endregion
+    }
+}
