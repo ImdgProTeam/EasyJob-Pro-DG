a565ad68e6c89e59a45f4515f1cb6fa356d3cac9
eblabs <eblabs@mail.ru>
2022-06-25 00:36:29 +0300

    DummyConverter added

---
+++ EasyJob Pro DG.UI/View/Converters/DummyConverter.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Globalization;
+
+namespace EasyJob_ProDG.UI.View.Converters
+{
+    internal class DummyConverter : ConverterBase
+    {
+        public override object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value;
+        }
+
+        public override object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value;
+        }
+    }
+}
