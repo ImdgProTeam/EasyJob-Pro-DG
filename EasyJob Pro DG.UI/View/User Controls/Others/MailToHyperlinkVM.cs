2b12b0df8087ac95f4a736da250e9e7973dfb1ec
eblabs <eblabs@mail.ru>
2022-07-05 09:58:24 +0300

    To the previos

---
+++ EasyJob Pro DG.UI/View/User Controls/Others/MailToHyperlinkVM.cs
@@ -0,0 +1,11 @@
+﻿using EasyJob_ProDG.Data;
+
+namespace EasyJob_ProDG.UI.View.User_Controls
+{
+    internal class MailToHyperlinkVM
+    {
+        public static string HyperlinkUri =>
+            $"mailto:feedback@imdg.pro?subject=ProDG Pro feedback (Version {ProgramDefaultSettingValues.ReleaseVersion})" +
+            $"&body=/Please attach your .edi and ShipProfile.ini files in case you experience problems or found an error in cargo data/";
+    }
+}
