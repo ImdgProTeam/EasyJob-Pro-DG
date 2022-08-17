a072dac904f815decba419742df4fce1b0889c0f
eblabs <eblabs@mail.ru>
2022-08-18 02:07:22 +0300

    Minor design changes and improvements

--- EasyJob Pro DG.UI/View/DialogWindows/DialogWindowOptions.xaml.cs
+++ EasyJob Pro DG.UI/View/DialogWindows/DialogWindowOptions.xaml.cs
@@ -1,11 +1,9 @@
-﻿using EasyJob_ProDG.UI.Services.DialogServices;
-
-namespace EasyJob_ProDG.UI.View.DialogWindows
+﻿namespace EasyJob_ProDG.UI.View.DialogWindows
 {
     /// <summary>
     /// Логика взаимодействия для dialogOpenFileOptions.xaml
     /// </summary>
-    public partial class DialogWindowOptions : AnimatedDialogResultWindow, IDialogWindow
+    public partial class DialogWindowOptions : AnimatedDialogResultWindow
     {
         public DialogWindowOptions()
         {
