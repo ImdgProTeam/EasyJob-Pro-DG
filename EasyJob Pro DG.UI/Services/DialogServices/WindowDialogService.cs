14db832e7be80b8375988a11baed9e04044936af
eblabs <eblabs@mail.ru>
2022-08-27 22:34:42 +0300

    Before implementing new functions to CargoReport

--- EasyJob Pro DG.UI/Services/DialogServices/WindowDialogService.cs
+++ EasyJob Pro DG.UI/Services/DialogServices/WindowDialogService.cs
@@ -7,11 +7,32 @@ namespace EasyJob_ProDG.UI.Services.DialogServices
     /// </summary>
     class WindowDialogService
     {
+        /// <summary>
+        /// Displays window.
+        /// </summary>
+        /// <param name="window"></param>
         public void ShowDialog(Window window)
         {
             window.ShowDialog();
         }
 
+        /// <summary>
+        /// Displays window with data bound to viewModel.
+        /// </summary>
+        /// <typeparam name="TViewModel">Type of view model class.</typeparam>
+        /// <param name="window">window to be displayed.</param>
+        /// <param name="viewModel">VM the window DataContext to bound to.</param>
+        public void ShowDialog<TViewModel>(Window window, TViewModel viewModel)
+            where TViewModel : class, new()
+        {
+            window.DataContext = viewModel;
+            window.ShowDialog();
+        }
+
+        /// <summary>
+        /// Closes window.
+        /// </summary>
+        /// <param name="window"></param>
         public void CloseDialog(Window window)
         {
             window.Close();
