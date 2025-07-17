27a36c719308c0b46300e2c26662d238c2875d5d
eblabs <eblabs@mail.ru>
2025-07-17 20:24:23 +0300

    Working version changed to 0.9.0.241

--- EasyJob Pro DG.Data/Properties/AssemblyInfo.cs
+++ EasyJob Pro DG.Data/Properties/AssemblyInfo.cs
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 // Можно задать все значения или принять номера сборки и редакции по умолчанию 
 // используя "*", как показано ниже:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.9.0.240")]
-[assembly: AssemblyFileVersion("0.9.0.240")]
+[assembly: AssemblyVersion("0.9.0.241")]
+[assembly: AssemblyFileVersion("0.9.0.241")]
