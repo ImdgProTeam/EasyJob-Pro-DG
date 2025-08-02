5f6bc27f364e84f8eb209f8e381c2ecd2b7bb501
eblabs <eblabs@mail.ru>
2025-08-02 21:59:35 +0300

    Shifted several .xaml files common styles to /Styles

--- EasyJob Pro DG.UI/View/User Controls/MainWindow/DataGridDg.xaml.cs
+++ EasyJob Pro DG.UI/View/User Controls/MainWindow/DataGridDg.xaml.cs
@@ -19,8 +19,8 @@ namespace EasyJob_ProDG.UI.View.User_Controls
     /// </summary>
     public partial class DataGridDg : CommonDataGridUserControl
     {
-        private double OriginalScrollPosition { get; set; }
-        private bool IsResizingColumn { get; set; }
+        //private double OriginalScrollPosition { get; set; }
+        //private bool IsResizingColumn { get; set; }
 
 
         public DataGridDg() : base(Animations.AnimationTypes.SlideAndFadeInFromLeft)
@@ -185,61 +185,61 @@ namespace EasyJob_ProDG.UI.View.User_Controls
 
         #region Scroll logic
 
-        private void DataGrid_ScrollChanged(object sender, ScrollChangedEventArgs e)
-        {
-            var dataGrid = sender as DataGrid;
-            if (this.IsResizingColumn
-              && TryFindVisualChildElement(dataGrid, out ScrollViewer scrollViewer))
-            {
-                this.Dispatcher.InvokeAsync(() =>
-                {
-                    scrollViewer.ScrollToHorizontalOffset(this.OriginalScrollPosition);
-                    this.IsResizingColumn = false;
-                }, DispatcherPriority.Background);
-            }
-        }
-        private void DataGridCell_SizeChanged(object sender, SizeChangedEventArgs e)
-        {
-            var dataGridCell = sender as DataGridCell;
-            if (TryFindVisualChildElement(this.MainDgTable, out ScrollViewer scrollViewer))
-            {
-                this.OriginalScrollPosition = scrollViewer.HorizontalOffset;
-                this.IsResizingColumn = true;
-            }
-        }
+        //private void DataGrid_ScrollChanged(object sender, ScrollChangedEventArgs e)
+        //{
+        //    var dataGrid = sender as DataGrid;
+        //    if (this.IsResizingColumn
+        //      && TryFindVisualChildElement(dataGrid, out ScrollViewer scrollViewer))
+        //    {
+        //        this.Dispatcher.InvokeAsync(() =>
+        //        {
+        //            scrollViewer.ScrollToHorizontalOffset(this.OriginalScrollPosition);
+        //            this.IsResizingColumn = false;
+        //        }, DispatcherPriority.Background);
+        //    }
+        //}
+        //private void DataGridCell_SizeChanged(object sender, SizeChangedEventArgs e)
+        //{
+        //    var dataGridCell = sender as DataGridCell;
+        //    if (TryFindVisualChildElement(this.MainDgTable, out ScrollViewer scrollViewer))
+        //    {
+        //        this.OriginalScrollPosition = scrollViewer.HorizontalOffset;
+        //        this.IsResizingColumn = true;
+        //    }
+        //}
 
-        private bool TryFindVisualChildElement<TChild>(DependencyObject parent, out TChild resultElement)
-          where TChild : DependencyObject
-        {
-            resultElement = null;
+        //private bool TryFindVisualChildElement<TChild>(DependencyObject parent, out TChild resultElement)
+        //  where TChild : DependencyObject
+        //{
+        //    resultElement = null;
 
-            if (parent is Popup popup)
-            {
-                parent = popup.Child;
-                if (parent == null)
-                {
-                    return false;
-                }
-            }
+        //    if (parent is Popup popup)
+        //    {
+        //        parent = popup.Child;
+        //        if (parent == null)
+        //        {
+        //            return false;
+        //        }
+        //    }
 
-            for (var childIndex = 0; childIndex < VisualTreeHelper.GetChildrenCount(parent); childIndex++)
-            {
-                DependencyObject childElement = VisualTreeHelper.GetChild(parent, childIndex);
+        //    for (var childIndex = 0; childIndex < VisualTreeHelper.GetChildrenCount(parent); childIndex++)
+        //    {
+        //        DependencyObject childElement = VisualTreeHelper.GetChild(parent, childIndex);
 
-                if (childElement is TChild child)
-                {
-                    resultElement = child;
-                    return true;
-                }
+        //        if (childElement is TChild child)
+        //        {
+        //            resultElement = child;
+        //            return true;
+        //        }
 
-                if (TryFindVisualChildElement(childElement, out resultElement))
-                {
-                    return true;
-                }
-            }
+        //        if (TryFindVisualChildElement(childElement, out resultElement))
+        //        {
+        //            return true;
+        //        }
+        //    }
 
-            return false;
-        }
+        //    return false;
+        //}
 
         #endregion
 
