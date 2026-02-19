9ca2b5621bd5450c4e00cdea536085d318bb6be0
eblabs <eblabs@mail.ru>
2026-02-19 23:14:46 +0300

    Fixed: excel template settings - rows could be deleted by pressing del button

--- EasyJob Pro DG.UI/Properties/AssemblyInfo.cs
+++ EasyJob Pro DG.UI/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Windows;
 [assembly: AssemblyConfiguration("Desktop version")]
 [assembly: AssemblyCompany("imdg.pro")]
 [assembly: AssemblyProduct("Pro DG Pro")]
-[assembly: AssemblyCopyright("Copyright ©  2018 - 2025")]
+[assembly: AssemblyCopyright("Copyright ©  2018 - 2026")]
 [assembly: AssemblyTrademark("imdg.pro")]
 [assembly: AssemblyCulture("")]
 
