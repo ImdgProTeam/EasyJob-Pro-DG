063b8397b602a37ab8b792746faab3129e717656
eblabs <eblabs@mail.ru>
2026-05-20 00:14:06 +0300

    Added few items to ProperShippingName context menu.

--- EasyJob Pro DG.UI/ViewModel/MainWindow/DataGridDgContextMenuViewModel.cs
+++ EasyJob Pro DG.UI/ViewModel/MainWindow/DataGridDgContextMenuViewModel.cs
@@ -9,9 +9,11 @@ namespace EasyJob_ProDG.UI.ViewModel
 
         const string PSN_LTDQTY = "LIMITED QUANTITY";
         const string PSN_MAX1L = "MAXIMUM 1 LITRE";
-        const string PSN_STABILIZED = "STABILIZED";
         const string PSN_WASTE = "WASTE";
+        const string PSN_STABILIZED = "STABILIZED";
+        const string PSN_MOLTEN = "MOLTEN";
         const string PSN_EMPTY_UNCLEANED = "EMPTY UNCLEANED";
+        const string PSN_RESIDUE = "RESIDUE LAST CONTAINED";
         const string PSN_COOLANT = "AS COOLANT";
         const string PSN_CONDITIONER = "AS CONDITIONER";
 
@@ -27,9 +29,11 @@ namespace EasyJob_ProDG.UI.ViewModel
 
         public string HeaderTitleLTDQTY => PSN_LTDQTY;
         public string HeaderTitleMAX1L => PSN_MAX1L;
-        public string HeaderTitleSTABILIZED => PSN_STABILIZED;
         public string HeaderTitleWASTE => PSN_WASTE;
+        public string HeaderTitleSTABILIZED => PSN_STABILIZED;
+        public string HeaderTitleMOLTEN => PSN_MOLTEN;
         public string HeaderTitleEMPTY_UNCLEANED => PSN_EMPTY_UNCLEANED;
+        public string HeaderTitleRESIDUE => PSN_RESIDUE;
         public string HeaderTitleCOOLANT => PSN_COOLANT;
         public string HeaderTitleCONDITIONER => PSN_CONDITIONER;
 
@@ -43,9 +47,11 @@ namespace EasyJob_ProDG.UI.ViewModel
         public bool IsTechnicalNameOfSelectedDgIncluded => SelectedDg?.IsTechnicalNameIncluded ?? false;
         public bool IsToNameIncludedLimitedQuantity => SelectedDg?.Name.Contains(PSN_LTDQTY) ?? false;
         public bool IsToNameIncludedMax1l => SelectedDg?.Name.Contains(PSN_MAX1L) ?? false;
-        public bool IsToNameIncludedStabilized => SelectedDg?.Name.Contains(PSN_STABILIZED) ?? false;
         public bool IsToNameIncludedWaste => SelectedDg?.Name.Contains(PSN_WASTE) ?? false;
+        public bool IsToNameIncludedStabilized => SelectedDg?.Name.Contains(PSN_STABILIZED) ?? false;
+        public bool IsToNameIncludedMolten => SelectedDg?.Name.Contains(PSN_MOLTEN) ?? false;
         public bool IsToNameIncludedEmptyUncleaned => SelectedDg?.Name.Contains(PSN_EMPTY_UNCLEANED) ?? false;
+        public bool IsToNameIncludedResidue => SelectedDg?.Name.Contains(PSN_RESIDUE) ?? false;
         public bool IsToNameIncludedAsCoolant => SelectedDg?.Name.Contains(PSN_COOLANT) ?? false;
         public bool IsToNameIncludedAsConditioner => SelectedDg?.Name.Contains(PSN_CONDITIONER) ?? false; 
 
@@ -88,14 +94,20 @@ namespace EasyJob_ProDG.UI.ViewModel
                 case PSN_MAX1L:
                     isIncluded = IsToNameIncludedMax1l;
                     break;
-                case PSN_STABILIZED:
-                    isIncluded = IsToNameIncludedStabilized;
-                    break;
                 case PSN_WASTE:
                     isIncluded = IsToNameIncludedWaste;
                     break;
+                case PSN_STABILIZED:
+                    isIncluded = IsToNameIncludedStabilized;
+                    break;
+                case PSN_MOLTEN:
+                    isIncluded = IsToNameIncludedMolten;
+                    break;
                 case PSN_EMPTY_UNCLEANED:
                     isIncluded = IsToNameIncludedEmptyUncleaned;
+                    break;
+                case PSN_RESIDUE:
+                    isIncluded = IsToNameIncludedResidue;
                     break;
                 case PSN_COOLANT:
                     isIncluded = IsToNameIncludedAsCoolant;
