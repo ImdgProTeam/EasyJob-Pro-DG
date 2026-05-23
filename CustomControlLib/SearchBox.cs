c0139e0fee703bb174e4b127554576cfc15b8b62
eblabs <eblabs@mail.ru>
2026-05-24 02:17:49 +0300

    Fix to the previous commit: ContextMenu placement of SettingsButton was not bound to the button itself and the menu popped up at wrong position. Same also resulted in Properties not being bound until the first right-click on the button.

--- CustomControlLib/SearchBox.cs
+++ CustomControlLib/SearchBox.cs
@@ -83,6 +83,9 @@ namespace CustomControlLib
         {
             if (sender is Button button && button.ContextMenu != null)
             {
+                button.ContextMenu.PlacementTarget = button;
+                button.ContextMenu.Placement = System.Windows.Controls.Primitives.PlacementMode.Bottom;
+
                 button.ContextMenu.IsOpen = true;
             }
         }
