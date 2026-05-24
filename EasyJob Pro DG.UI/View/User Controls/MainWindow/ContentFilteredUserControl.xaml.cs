387a0b0fd00897c736d42b1888c5e545d4ef235b
eblabs <eblabs@mail.ru>
2026-05-24 03:31:30 +0300

    DataGrids: Button 'Content below is filtered' shifted to a separate ContentFilteredUserControl

---
+++ EasyJob Pro DG.UI/View/User Controls/MainWindow/ContentFilteredUserControl.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace EasyJob_ProDG.UI.View.User_Controls
+{
+    /// <summary>
+    /// Interaction logic for ContentFilteredUserControl.xaml
+    /// </summary>
+    public partial class ContentFilteredUserControl : UserControl
+    {
+        public ContentFilteredUserControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
